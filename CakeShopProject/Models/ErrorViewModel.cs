879c2a02de37027077371f4d561401c38cefd41f
PC <PC@LAPTOP-1E78MOEK>
2024-06-18 06:45:15 +0700

    Add project files.

---
+++ CakeShopProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CakeShopProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
